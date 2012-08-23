f6b24f10af8a2b45732f653d8b22d3f412ee097a
Kenichi Nakamura <kenichi.nakamura@gmail.com>
2012-08-23 14:11:18 -0700

    Initial Commit

---
+++ Geotracks/LQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LQAppDelegate.h
+//  Geotracks
+//
+//  Created by Kenichi Nakamura on 8/23/12.
+//  Copyright (c) 2012 Geoloqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Geotracks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Geotracks
+//
+//  Created by Kenichi Nakamura on 8/23/12.
+//  Copyright (c) 2012 Geoloqi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LQAppDelegate class]));
+    }
+}
